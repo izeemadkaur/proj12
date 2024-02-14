34c8f5e05de61b2c164981e03dd2a9a57cd007b6
Izee Madkaur <imadkaur@gtlic.com>
2024-02-14 08:48:17 -0600

    made updates

--- src/app/underwriting-questions/page.tsx
+++ src/app/underwriting-questions/page.tsx
@@ -13,7 +13,6 @@ const questions = [
   { question: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?`, option1: "Yes", option2: "No" },
   { question: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?`, option1: "Yes", option2: "No" },
   { question: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?`, option1: "Yes", option2: "No" },
-  // Add more questions as needed
 ];
 
 export default function Page() {
